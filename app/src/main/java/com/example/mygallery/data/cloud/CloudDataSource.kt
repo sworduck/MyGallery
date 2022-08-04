11eee604b5134f4e6a7dc08a166ec4a5a484c46a
sworduck <iwanovmicha6@gmail.com>
2022-08-04 15:02:18 +0400

    refactoring

--- app/src/main/java/com/example/mygallery/data/cloud/CloudDataSource.kt
+++ app/src/main/java/com/example/mygallery/data/cloud/CloudDataSource.kt
@@ -3,7 +3,6 @@ package com.example.mygallery.data.cloud
 import javax.inject.Inject
 
 class CloudDataSource @Inject constructor(private val apiService: ApiService) {
-
     fun getPictureCloudPagingSource(): PictureCloudPagingSource {
         return PictureCloudPagingSource(apiService)
     }
